1f6e4748a457b1878416c128f66fa6b3ca1acb5a
rushike <rushike.ab1@gmail.com>
2020-09-28 19:00:25 +0530

    so many changes, just pull

--- runner.py
+++ runner.py
@@ -3,14 +3,14 @@ import argparse
 whatsapp.sleeptime = 0.001
 
 parser = argparse.ArgumentParser(description='Whatsapp Annoyer Guide')
-parser.add_argument('--chrome_driver_path', dest = "chromedriver_path", action='store', type=str, default='./driver/chromedriver.exe', help='chromedriver executable path (MAC and Windows path would be different)')
+parser.add_argument('--chrome_driver_path', dest = "chromedriver_path", action='store', type=str, default='./chromedriver.exe', help='chromedriver executable path (MAC and Windows path would be different)')
 parser.add_argument('--message', dest = "message",action='store', type=str, default='', help='Enter the msg you want to send')
 parser.add_argument('--contact', dest = "contact",action='store', type=str, default='', help='Enter the Contact Name[Exact. Case Sensitive]')
 parser.add_argument('--times', dest = "times", action='store', type=int, default=1, help='Enter the no. of time you want to send same message')
 args = parser.parse_args()
 
 def ask_questions():
-    args.contact = '"' + input("%-50s"%("Enter the contact name [case sensetive] : ")) + '"'
+    args.contact = input("%-50s"%("Enter the contact name [case sensetive] : "))
     args.message = input("%-50s"%("Enter the message : "))
     args.times = int(input("%-50s"%("Enter how many time you wanna annoy : ")))
     cpath = input(f"Enter driver path, [default : {args.chromedriver_path}] : ")
